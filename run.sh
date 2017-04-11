de3f6cdb355e28b5428861af61b674e6b042b950
Jaap van Blaaderen <jbl@woodwing.com>
2017-04-11 17:05:44 +0200

    Add run.sh to easily start the server with the required environment variables

---
+++ run.sh
@@ -0,0 +1,7 @@
+export SAMPLE_APP_PORT='9080'
+export ELVIS_SERVER_URL='http://localhost:8080'
+export ELVIS_USERNAME='admin'
+export ELVIS_PASSWORD='changemenow'
+export WEBHOOK_SECRET_TOKEN=''
+
+npm start
